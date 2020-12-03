957502edbfe0cbf2c15a0726648dd9c56541806a
Adam J. Bauman <adam@kungfutreachery.net>
2020-12-02 22:57:24 -0600

    Cleanup

--- scratch_connect.py
+++ scratch_connect.py
@@ -11,9 +11,10 @@ if __debug__:
 import os
 import pygame
 import sys, getopt
+#import random
+#import requests
+from collections import deque
 import threading
-import random
-import requests
 
 # Simple check for RPi GPIO, will disable any stuff that requires GPIO access so you can
 # debug and develop on other platforms.
@@ -28,28 +29,57 @@ except:
 if g_gpio_available:
     from data.dht22 import DHT22, DHT22Data
 
-# TODO: (Adam) 2020-12-1 Conditionally load modules that require GPIO for easier development
-#            and debugging on non-GPIO equipped platforms.
 from data.aida64lcdsse import AIDA64LCDSSE
-#from data.dht22 import DHT22
 
 from elements.styles import FontPaths, Color
 from dashpages import DashPage1Painter
 from elements.styles import Color, AssetPath, FontPaths
-
 #from utilities.screensaver import MatrixScreensaver
-
-from collections import deque
-import threading
-
-#from sseclient import SSEClient
 
 
 class Hardware:
     screen_width = 480
     screen_height = 320
+
+def print_usage():
+    print("")
+    print("Usage: neuromancer_dash.py <options>")
+    print("Example: python3 neuromancer_dash.py --aidasse http://localhost:8080/sse")
+    print("")
+    print("       Required Options:")
+    print("           --aidasse <full http address:port to AIDA64 LCD SSE stream>")
+
+def get_command_args(argv):
+    aida_sse_server = None
+    gpio_enabled = True
+
+    try:
+        opts, args = getopt.getopt(argv,"aidasse:",["aidasse=", ])
+
+    except getopt.GetoptError:
+        print_usage()
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print_usage()
+            sys.exit()
+        elif opt in ("--aidasse"):
+            aida_sse_server = arg
+
+    if (None == aida_sse_server):
+        print_usage()
+        sys.exit()
+
+    return aida_sse_server
         
 def main(argv):
+    aida_sse_server = get_command_args(argv)
+    assert(None != aida_sse_server)
+
+    if __debug__:
+        print("Passed arguments:")
+        print("    aidasse = {}".format(aida_sse_server))
 
     pygame.init()
     pygame.mixer.quit()
@@ -72,8 +102,7 @@ def main(argv):
     # Start the AIDA64 data thread, fastest update interval is usually ~100ms and can be
     # adjusted in the AIDA64 preferences. 
     aida64_deque = deque([], maxlen=data_queue_maxlen)
-    server_address = "http://localhost:8080/sse"
-    aida64_data_thread = threading.Thread(target=AIDA64LCDSSE.threadable_stream_read, args=(aida64_deque, server_address))
+    aida64_data_thread = threading.Thread(target=AIDA64LCDSSE.threadable_stream_read, args=(aida64_deque, aida_sse_server))
     aida64_data_thread.setDaemon(True)
     aida64_data_thread.start()
 
@@ -107,42 +136,14 @@ def main(argv):
             time.sleep(0.050)
             continue
 
-        # Paint the updated dashboard page and flip the display.
+        # Paint the updated dashboard page and flip the display. DHT22 data will not be available if
+        # the system running the script doesn't have RPi.GPIO support.
         if None == dht22_deque:
             dash_page_1_painter.paint(aida64_deque.popleft(), None)
         else:
             dash_page_1_painter.paint(aida64_deque.popleft(), dht22_deque.popleft())
 
         pygame.display.flip()
-
-        #leftmost_data = aida64_deque.popleft()
-        #print("Data_in_main_thread: {}".format(leftmost_data))
-        ##print("Remaining length: {}".format(len(data)))
-
-
-    print("Exited main loop")
-    lcd_thread.join()
-
-    #aida64_lcd.get()
-
-    #while True:
-    #    display_surface.fill(Color.black)
-
-    #    display_surface.blit(disk_activity.update(data), (20,20))
-        
-    #    for event in pygame.event.get():
-    #        if event.type == pygame.QUIT:
-    #            print("User quit")
-    #            pygame.quit()
-    #            sys.exit()
-
-    #    pygame.event.clear()
-
-    #    pygame.display.flip()
-
-    #    time.sleep(0.100)
-
-    #pygame.quit()
 
 
 if __name__ == "__main__":
