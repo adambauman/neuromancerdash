2b1894d40c15c3f073a9588a1078a6c63228266d
Adam J. Bauman <adam@kungfutreachery.net>
2020-12-09 20:52:28 -0600

    Preparing to split page sections into subsurfaces

--- scratch.py
+++ scratch.py
@@ -18,43 +18,36 @@ def main():
         (Hardware.screen_width, Hardware.screen_height),
         pygame.HWSURFACE | pygame.DOUBLEBUF
     )
-    display_surface.fill(Color.cyan_dark)
+    display_surface.fill(Color.red)
 
 
-    # Setup our stuff
-    #working_surface = pygame.Surface((Hardware.screen_width, Hardware.screen_height), pygame.SRCALPHA)
-    display_surface.set_alpha(255)
-    working_surface = display_surface.subsurface(50, 50, 200, 200)
-    working_surface.fill((0,0,255,100))
+    # This will be the base serface that's flipped every update
+    working_surface = pygame.Surface((Hardware.screen_width, Hardware.screen_height), pygame.SRCALPHA)
+    working_surface.fill(Color.cyan_dark)
+
     test_font = pygame.freetype.Font(FontPaths.fira_code_semibold(), 14)
     test_font.kerning = True
-        
-    origin = (20, 20)
-    value_field_rect = pygame.Rect(origin[0], origin[1], 100, test_font.get_sized_height())
-    print("value_field_rect; {}".format(value_field_rect))
-    test_subsurface = working_surface.subsurface(value_field_rect)
-    
-    display_flags = display_surface.get_flags()
-    working_flags = working_surface.get_flags()
-    testsubsurface_flags = test_subsurface.get_flags()
+
+    ambient_rect = pygame.Rect(50, 50, 100, 100)
+    ambient_surface = working_surface.subsurface(ambient_rect)
+
+    working_origin = (0, 0)
+    test_font.render_to(ambient_surface, working_origin, "Temp", Color.white)
+
+    working_origin = (0, working_origin[1] + test_font.get_sized_height())
+    value_rect = pygame.Rect(working_origin[0], working_origin[1], ambient_surface.get_width(), test_font.get_sized_height())
+    print("value_rect: {}".format(value_rect))
+    value_surface = ambient_surface.subsurface(value_rect)
+    value_original_bg = value_surface.get_abs_parent().copy()
 
     # Count down from number, should clear pixels and not have overlap
     for value in range(110, 0, -1):
-        #test_pxarray = pygame.PixelArray(test_subsurface)
-        ##print("shape0: {}    shape1: {}".format(test_pxarray.shape[0], test_pxarray.shape[1]))
-        #for array_x in range(0, test_pxarray.shape[0]):
-        #    for array_y in range(0, test_pxarray.shape[1]):
-        #        #print("x: {}, y: {}".format(array_x, array_y))
-        #        test_pxarray[array_x, array_y] = (0,0,0,0)
-        #test_pxarray.close()
-
-        test_subsurface.fill((0,0,0,0))
+        value_surface.blit(value_original_bg, (0, 0))
 
         print("value: {}".format(value))
-        test_font.render_to(test_subsurface, (0,0), "Test: {}".format(value), Color.white)
-        #test_font.render_to(working_surface, origin, "Test: {}".format(value), Color.white)
+        test_font.render_to(value_surface, (0,0), "{} F".format(value), Color.yellow)
         
-        #display_surface.blit(working_surface, (0,0))
+        display_surface.blit(working_surface, (0, 0))
         pygame.display.flip()
 
         for event in pygame.event.get():
